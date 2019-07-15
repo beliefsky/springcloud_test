4f23c07024d28a3339f463754b3dcbb627939880
weijing <ibeliefsky@gmail.com>
2019-07-15 22:01:04 +0800

    none

--- sky-config-server/src/main/java/com/sky/ConfigServerApplication.java
+++ sky-config-server/src/main/java/com/sky/ConfigServerApplication.java
@@ -2,12 +2,12 @@ package com.sky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
