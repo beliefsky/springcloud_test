0a770e7869d2f3872a64b77c108a5e7b876172ce
weijing <ibeliefsky@gmail.com>
2019-08-02 23:34:51 +0800

    httpclient pool

--- sky-web/src/main/java/com/sky/demo/controller/TestController.java
+++ sky-web/src/main/java/com/sky/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.sky.demo.controller;
 
+import com.sky.demo.HttpConnectionPoolUtil;
 import com.sky.demo.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,8 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
+        String result = HttpConnectionPoolUtil.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4a11ae335e843db1&secret=09069ff8aa678e0262b397b65dd0d43e&code=001uPrcU04M8TY1XbT8U0yS1cU0uPrc6&grant_type=authorization_code");
+        System.out.println(result);
         return testService.test();
     }
 }
